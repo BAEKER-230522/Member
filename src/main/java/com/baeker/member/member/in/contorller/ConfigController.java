1f2fa62a834f3b11ab2ad261299b45316833b224
choideakook <shdrnrhd113@naver.com>
2023-05-23 00:16:45 +0900

    d

--- src/main/java/com/baeker/member/member/in/contorller/ConfigController.java
+++ src/main/java/com/baeker/member/member/in/contorller/ConfigController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
  * Config Server 와 연결 상태를 확인하는 Controller 입니다.
  */
 @RestController
-//@RefreshScope
 @Slf4j
 public class ConfigController {
 
