608e5408411f19f1bee2805d975742b8a25bcd1c
choideakook <shdrnrhd113@naver.com>
2023-06-19 10:07:06 +0900

    feat: jasypt 암호화, 복호화 코드 구현

---
+++ src/test/java/com/baeker/member/base/util/JasyptConfigTest.java
@@ -0,0 +1,37 @@
+package com.baeker.member.base.util;
+
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.jasypt.util.text.BasicTextEncryptor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JasyptConfigTest {
+
+    @Value("${jasypt.encryptor.password}")
+    private String key;
+
+
+
+    @Test
+    void 암호화() {
+        String value = "root";
+        System.out.println(jasyptEncoding(value));
+    }
+
+    @Test
+    void 복호화() {
+        String value = "V7ZQ6ejRGHvYoSUQbPa4mrc5dxRDh6IKg1Z9tLyOKmY=";
+        BasicTextEncryptor enc = new BasicTextEncryptor();
+        enc.setPassword(key);
+        System.out.println(enc.decrypt(value));
+    }
+
+    public String jasyptEncoding(String value) {
+        StandardPBEStringEncryptor enc = new StandardPBEStringEncryptor();
+        enc.setAlgorithm("PBEWithMD5AndDES");
+        enc.setPassword(key);
+        return enc.encrypt(value);
+    }
+}
