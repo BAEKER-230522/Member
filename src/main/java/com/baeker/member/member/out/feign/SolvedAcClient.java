d821af5a5eb71f0f5e48e8a45d0b055689af4109
choideakook <shdrnrhd113@naver.com>
2023-06-29 09:42:19 +0900

    docs: application dev 추가

--- src/main/java/com/baeker/member/member/out/feign/SolvedAcClient.java
+++ src/main/java/com/baeker/member/member/out/feign/SolvedAcClient.java
@@ -3,6 +3,7 @@ package com.baeker.member.member.out.feign;
 import com.baeker.member.base.request.RsData;
 import com.baeker.member.member.out.resDto.ConBaekjoonResDto;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
