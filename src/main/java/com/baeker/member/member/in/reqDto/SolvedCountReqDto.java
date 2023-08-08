eae08bd90d9f477bfc31bfe00fd0fbfeb6ae5968
I'm back <115536240+choideakook@users.noreply.github.com>
2023-08-08 22:54:48 +0900

    feat: kafka 임시 대용 member solved count update api 구현 (#56)

---
+++ src/main/java/com/baeker/member/member/in/reqDto/SolvedCountReqDto.java
@@ -0,0 +1,14 @@
+package com.baeker.member.member.in.reqDto;
+
+import lombok.Data;
+
+@Data
+public class SolvedCountReqDto {
+    private Long id;
+    private int bronze;
+    private int silver;
+    private int gold;
+    private int diamond;
+    private int ruby;
+    private int platinum;
+}
