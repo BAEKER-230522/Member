3d6c4c552539b54668d50dba98854a09d3a9de60
PARKPARKWOO <wy9295@naver.com>
2023-09-10 03:43:03 +0900

    Feat: 소셜 로그인 code 와 리다이렉트 uri입력받고 회원가입

---
+++ src/main/java/com/baeker/member/base/security/oauth2/controller/KakaoController.java
@@ -0,0 +1,38 @@
+package com.baeker.member.base.security.oauth2.controller;
+
+import com.baeker.member.base.security.jwt.JwtTokenProvider;
+import com.baeker.member.base.security.oauth2.model.providers.ProviderUser;
+import com.baeker.member.base.security.oauth2.service.CustomOAuth2UserService;
+import com.baeker.member.base.security.oauth2.service.KakaoService;
+import com.baeker.member.member.domain.entity.Member;
+import com.baeker.member.member.domain.service.MemberService;
+import com.baeker.member.member.in.resDto.JwtTokenResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/oauth2/authorization")
+public class KakaoController {
+
+    private final CustomOAuth2UserService oAuth2UserService;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final MemberService memberService;
+    private final KakaoService kakaoService;
+
+    @GetMapping("/kakao")
+    public void kakaoLogin(String code, String redirectUri) {
+        kakaoService.kakaoLogin(code, redirectUri);
+
+//        String name = oAuth2User.getName();
+//        Member byUsername = memberService.findByUsername(name);
+//        Map<String, String> mapToken = jwtTokenProvider.genAccessTokenAndRefreshToken(byUsername);
+//        return new JwtTokenResponse(mapToken.get("accessToken"), mapToken.get("refreshToken"));
+    }
+}
--- src/main/java/com/baeker/member/base/security/oauth2/service/KakaoService.java
+++ src/main/java/com/baeker/member/base/security/oauth2/service/KakaoService.java
@@ -1,12 +1,36 @@
 package com.baeker.member.base.security.oauth2.service;
 
 import com.baeker.member.member.in.resDto.JwtTokenResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.security.Provider;
 
 @Service
+@RequiredArgsConstructor
 public class KakaoService {
+    private final CustomOidcUserService oidcUserService;
 
-//    public JwtTokenResponse kakaoLogin(String code) {
-//
-//    }
+    @Value("${spring.security.oauth2.client.registration.kakao.clientId}")
+    private String clientId;
+    @Value("${spring.security.oauth2.client.registration.kakao.client-secret}")
+    private String clientSecret;
+    @Value("${spring.security.oauth2.client.registration.kakao.authorization-grant-type}")
+    private String authorizationGrantType;
+    @Value("${spring.security.oauth2.client.provider.kakao.token-uri}")
+    private String tokenUri;
+    public void kakaoLogin(String code, String redirectUri) {
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+        String body = "grant_type=" + authorizationGrantType + "&client_id=" + clientId + "&redirect_uri=" + redirectUri + "&code=" + code + "&client_secret=" + clientSecret;
+        HttpEntity<String> request = new HttpEntity<>(body, headers);
+        String response = restTemplate.postForObject(tokenUri, request, String.class);
+        System.out.println(response);
+    }
 }
