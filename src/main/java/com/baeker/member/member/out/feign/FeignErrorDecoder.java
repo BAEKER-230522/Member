a4328071cc8bb527257ab1ed8e4c248a3237443d
PARKPARKWOO <wy9295@naver.com>
2023-09-26 23:28:27 +0900

    Delete FeignDecoder에 유레카 설정 삭제

--- src/main/java/com/baeker/member/member/out/feign/FeignErrorDecoder.java
+++ src/main/java/com/baeker/member/member/out/feign/FeignErrorDecoder.java
@@ -3,7 +3,6 @@ package com.baeker.member.member.out.feign;
 import com.baeker.member.base.error.exception.NotFoundException;
 import feign.Response;
 import feign.codec.ErrorDecoder;
-import jakarta.ws.rs.InternalServerErrorException;
 
 public class FeignErrorDecoder implements ErrorDecoder {
     @Override
@@ -12,8 +11,6 @@ public class FeignErrorDecoder implements ErrorDecoder {
         switch (response.status()) {
             case 400 :
                 return new NotFoundException("feign: data 를 찾을 수 없습니다.");
-            case  500 :
-                return new InternalServerErrorException("서버 내부에 오류가 발생했습니다.");
         }
         return new Exception(response.reason());
     }
