c17892f56d0386e981ce58c6eba40c6cfcd42165
PARKPARKWOO <wy9295@naver.com>
2023-10-02 18:06:25 +0900

    Refactor 불필요한 주석 삭제 및 메서드 분리

--- src/main/java/com/baeker/member/base/security/oauth2/controller/KakaoController.java
+++ src/main/java/com/baeker/member/base/security/oauth2/controller/KakaoController.java
@@ -1,23 +1,11 @@
 package com.baeker.member.base.security.oauth2.controller;
 
-import com.baeker.member.base.security.jwt.JwtTokenProvider;
-import com.baeker.member.base.security.oauth2.model.providers.ProviderUser;
-import com.baeker.member.base.security.oauth2.service.CustomOAuth2UserService;
 import com.baeker.member.base.security.oauth2.service.KakaoService;
 import com.baeker.member.base.security.oauth2.users.dto.SocialLoginResponse;
-import com.baeker.member.member.domain.entity.Member;
-import com.baeker.member.member.domain.service.MemberService;
-import com.baeker.member.member.in.resDto.JwtTokenResponse;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -29,9 +17,5 @@ public class KakaoController {
     @GetMapping("/kakao")
     public SocialLoginResponse kakaoLogin(String code, String redirectUri) {
         return kakaoService.kakaoLogin(code, redirectUri);
-//        String name = oAuth2User.getName();
-//        Member byUsername = memberService.findByUsername(name);
-//        Map<String, String> mapToken = jwtTokenProvider.genAccessTokenAndRefreshToken(byUsername);
-//        return new JwtTokenResponse(mapToken.get("accessToken"), mapToken.get("refreshToken"));
     }
 }
